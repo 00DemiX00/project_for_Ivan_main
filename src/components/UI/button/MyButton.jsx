499b5be44b3cfd97a68021456584e1a20569b752
00DemiX00 <alescha.yarkin@yandex.ru>
2025-03-10 20:34:44 +0300

    Внесены изменения в компонент MyButton

--- src/components/UI/button/MyButton.jsx
+++ src/components/UI/button/MyButton.jsx
@@ -4,7 +4,7 @@ import classes from './MyButton.module.css';
 
 const MyButton = ({children, ...props}) => {
     return (
-        <button className={classes.MyBtn}>
+        <button className={classes.myBtn}>
             {children}
         </button>
     );
