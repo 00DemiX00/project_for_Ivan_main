0eb5f766ef057f99c57d285841927b1ef3876c52
00DemiX00 <alescha.yarkin@yandex.ru>
2025-07-14 17:59:53 +0300

    fix: реструктуризация компонента MyForm

--- src/components/form/MyForm.jsx
+++ src/components/form/MyForm.jsx
@@ -1,41 +1,62 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import classes from './MyForm.module.css';
 import MyButton from "../UI/button/MyButton";
 import axios from "axios";
 
 const MyForm = (props) => {
-    const [inputValue, setInputValue] = useState('')
-    const [inputValue1, setInputValue1] = useState('')
+    //Состояния для хранения значений полей
+    const [inputValue, setInputValue] = useState('') //Логин
+    const [inputValue1, setInputValue1] = useState('') //Пароль
+    //Обаботчик отправки формы
+    const handleSubmit = async (e) => {
+        e.preventDefault(); //Отключение перезагрузки страницы
+        console.log('Логин:', inputValue);
+        console.log('Пароль:', inputValue1);
+        try {
+        //Отправляем GET-запрос на сервер
+            const { data } = await axios({
+                url: 'https://jsonplaceholder.typicode.com/posts', //пример URL
+                headers: { "Content-type": "application/json" },
+                params: { login: inputValue, password: inputValue1 }, //передача данных через параметры URL
+                method: "GET",
+                data: null,
+             });
+             console.log('Ответ сервера:', data); //вывод ответа в консоль
+        } 
+        catch (e) {
+            console.error('Ошибка при запросе:', e); //обработка ошибок
+        }
+    };
 
     return (
-        <div {...props} className={classes.TextField}>
-            <input value={inputValue} onChange={e => setInputValue(e.target.value)} {...props} className={classes.MyInp} required></input>
+        <form onSubmit={handleSubmit} {...props} className={classes.TextField}>
+            {/* Поле для логина */}
+            <input
+                value={inputValue} 
+                onChange={e => setInputValue(e.target.value)} //обновление состояния при вводе
+                {...props} 
+                className={classes.MyInp} 
+                required>
+            </input>
             <label {...props} className={classes.MyPlaceholder}>Логин</label>
             <br></br><br></br>
-            <input value={inputValue1} onChange={e => setInputValue1(e.target.value)} {...props} className={classes.MyInp1} required></input>
+            {/* Поле для пароля */}
+            <input
+                type="password"
+                value={inputValue1} 
+                onChange={e => setInputValue1(e.target.value)} //обновление состояния при вводе
+                {...props} 
+                className={classes.MyInp1} 
+                required>
+            </input>
             <label {...props} className={classes.MyPlaceholder1}>Пароль</label>
             <br></br><br></br>
-            <MyButton
-                onClick={async () => 
-                    {console.log(inputValue, inputValue1);
-                        try {
-                            await axios({
-                            url: `https://jsonplaceholder.typicode.com/`,
-                            headers: {"Content-type": "application/json"},
-                            params: {inputValue, inputValue1},
-                            method: "GET",
-                            data: null
-                        }).then(({data}) => {
-                            return data;});
-                        } 
-                        catch (e) {
-                            console.log('ERROR', e)
-                        }
-                        
-                    }}>
-            Войти</MyButton>
-        </div>
-    );
-};
+            {/* Кнопка отправки формы */}
+            <MyButton type="submit">Войти</MyButton>
+        </form>
+    )
+}
+   
 
 export default MyForm;
+    
